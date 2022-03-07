8feb5821db8551bfa33a035e26466893e46e4606
elena_abrarova <evabrarova@gmail.com>
2022-03-07 18:48:03 +0300

    first step finished

---
+++ bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program.name('gendiff')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information');
+
+program.parse();
