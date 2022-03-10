0f1e12ca4d10cd38c217574ba0b28cda5c0a775e
elena_abrarova <evabrarova@gmail.com>
2022-03-10 18:55:50 +0300

    fourth step finished

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,13 +1,11 @@
 import _ from 'lodash';
 
-const compareObj = (obj1, obj2) => {
-  const keys = _.union(_.keys(obj1), _.keys(obj2));
-  const sortedKeys = _.sortBy(keys);
-  const removed = '  -';
-  const added = '  +';
-  const noChange = '   ';
-  const result = sortedKeys.reduce((acc, key) => {
-    const unchanged = `${noChange} ${key}: ${obj1[key]}`;
+const removed = '  -';
+const added = '  +';
+const noChange = '   ';
+
+const makeLine = (key, obj1, obj2, acc) => {
+  const unchanged = `${noChange} ${key}: ${obj1[key]}`;
     const changed1 = `${removed} ${key}: ${obj1[key]}`;
     const changed2 = `${added} ${key}: ${obj2[key]}`;
     if (_.has(obj1, key) && _.has(obj2, key)) {
@@ -18,7 +16,12 @@ const compareObj = (obj1, obj2) => {
       return acc.concat(changed1);
     }
     return acc.concat(changed2);
-  }, []);
+}
+
+const compareObj = (obj1, obj2) => {
+  const keys = _.union(_.keys(obj1), _.keys(obj2));
+  const sortedKeys = _.sortBy(keys);
+  const result = sortedKeys.reduce((acc, key) => (makeLine(key, obj1, obj2, acc)), []);
   return `{\n${result.join('\n')}\n}`;
 };
 
