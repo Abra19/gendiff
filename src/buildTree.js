af861a79801314e44d231af82ab27de7cf5f7eda
elena_abrarova <evabrarova@gmail.com>
2022-03-10 18:58:18 +0300

    fourth step finished

--- src/buildTree.js
+++ src/buildTree.js
@@ -6,17 +6,17 @@ const noChange = '   ';
 
 const makeLine = (key, obj1, obj2, acc) => {
   const unchanged = `${noChange} ${key}: ${obj1[key]}`;
-    const changed1 = `${removed} ${key}: ${obj1[key]}`;
-    const changed2 = `${added} ${key}: ${obj2[key]}`;
-    if (_.has(obj1, key) && _.has(obj2, key)) {
-      return (obj1[key] === obj2[key]) ? _.concat(acc, unchanged)
-        : _.concat(acc, changed1, changed2);
-    }
-    if (_.has(obj1, key) && !_.has(obj2, key)) {
-      return acc.concat(changed1);
-    }
-    return acc.concat(changed2);
-}
+  const changed1 = `${removed} ${key}: ${obj1[key]}`;
+  const changed2 = `${added} ${key}: ${obj2[key]}`;
+  if (_.has(obj1, key) && _.has(obj2, key)) {
+    return (obj1[key] === obj2[key]) ? _.concat(acc, unchanged)
+      : _.concat(acc, changed1, changed2);
+  }
+  if (_.has(obj1, key) && !_.has(obj2, key)) {
+    return acc.concat(changed1);
+  }
+  return acc.concat(changed2);
+};
 
 const compareObj = (obj1, obj2) => {
   const keys = _.union(_.keys(obj1), _.keys(obj2));
