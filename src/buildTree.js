96be987c120b84bf6cfb262ecac8470d7911f985
elena_abrarova <evabrarova@gmail.com>
2022-03-10 18:25:34 +0300

    fourth step finished

--- src/buildTree.js
+++ src/buildTree.js
@@ -3,9 +3,9 @@ import _ from 'lodash';
 const compareObj = (obj1, obj2) => {
   const keys = _.union(_.keys(obj1), _.keys(obj2));
   const sortedKeys = _.sortBy(keys);
-  const removed = `  -`;
-  const added = `  +`;
-  const noChange = `  `;
+  const removed = '  -';
+  const added = '  +';
+  const noChange = '   ';
   const result = sortedKeys.reduce((acc, key) => {
     const unchanged = `${noChange} ${key}: ${obj1[key]}`;
     const changed1 = `${removed} ${key}: ${obj1[key]}`;
