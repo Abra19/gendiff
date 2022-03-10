a23d05ef3e9527a01fb9e15d75a9ee3ac860bf40
elena_abrarova <evabrarova@gmail.com>
2022-03-10 19:36:14 +0300

    fourth step finished

--- src/buildTree.js
+++ src/buildTree.js
@@ -8,14 +8,14 @@ const makeLine = (key, obj1, obj2, acc) => {
   const unchanged = `${noChange} ${key}: ${obj1[key]}`;
   const changed1 = `${removed} ${key}: ${obj1[key]}`;
   const changed2 = `${added} ${key}: ${obj2[key]}`;
-  if (_.has(obj1, key) && _.has(obj2, key)) {
-    return (obj1[key] === obj2[key]) ? _.concat(acc, unchanged)
-      : _.concat(acc, changed1, changed2);
+  if (!_.has(obj1, key)) {
+    return acc.concat(changed2);
   }
-  if (_.has(obj1, key) && !_.has(obj2, key)) {
+  if (!_.has(obj2, key)) {
     return acc.concat(changed1);
   }
-  return acc.concat(changed2);
+  return (obj1[key] === obj2[key]) ? _.concat(acc, unchanged)
+    : _.concat(acc, changed1, changed2);
 };
 
 const compareObj = (obj1, obj2) => {
