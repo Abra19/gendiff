0a16309f172cc22fc48dc92c6090e5ff3a66a92f
elena_abrarova <evabrarova@gmail.com>
2022-03-10 22:53:15 +0300

    Five step finished

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import { extname } from 'path';
 import parsePath from './parsers.js';
-import { compareJSONObj, compareYMLObj } from './buildTree.js';
+import compareObj from './buildTree.js';
 
 const genDiff = (path1, path2) => {
   const extension = extname(path1);
   const format = (extension === '.yaml') ? '.yml' : extension;
   const file1 = parsePath(path1, format);
   const file2 = parsePath(path2, format);
-  switch (format) {
-    case '.json':
-      return compareJSONObj(file1, file2);
-    case '.yml':
-      return compareYMLObj(file1, file2);
-    default:
-      throw new Error(`format ${format} is not supported`);
-  }
+  return compareObj(file1, file2);
 };
 
 export default genDiff;
