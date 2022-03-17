ebb2cd0235acd2895709f77483933f0b11709442
elena_abrarova <evabrarova@gmail.com>
2022-03-17 14:27:33 +0300

    Seventh step finished

--- src/comparator.js
+++ src/comparator.js
@@ -8,28 +8,21 @@ const buildDiffTree = (obj1, obj2) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
     if (!_.has(obj1, key)) {
-      return {
-        keyName: key, type: 'added', value: value2,
-      };
+      return { keyName: key, type: 'added', value: value2 };
     }
     if (!_.has(obj2, key)) {
-      return {
-        keyName: key, type: 'removed', value: value1,
-      };
+      return { keyName: key, type: 'removed', value: value1 };
     }
     if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      return {
-        keyName: key, type: 'hasChild', value: buildDiffTree(value1, value2),
-      };
+      return { keyName: key, type: 'hasChild', value: buildDiffTree(value1, value2) };
     }
     if (value1 === value2) {
-      return {
-        keyName: key, type: 'unchanged', value: value1,
-      };
+      return { keyName: key, type: 'unchanged', value: value1 };
     }
-    return {
+    const result = {
       keyName: key, type: 'changed', value: value1, changedValue: value2,
     };
+    return result;
   });
   return resultObj;
 };
